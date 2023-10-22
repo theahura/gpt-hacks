8a2a5369ea52388ca91c07cd4d4c77cad94dccc9
Amol Kapoor <theahura@gmail.com>
2023-10-22 19:36:06 -0400

    Style

--- src/base_skills.py
+++ src/base_skills.py
@@ -8,28 +8,41 @@ from typing import List, Any, Tuple
 
 def GetListOfRestaurantsAt(gps_coordinate: Tuple[float, float]) -> List[str]:
   return [
-    "BestPizza-%s" % location,
-    "BestRamen-%s" % location,
-    "BestPub-%s" % location,
-    "BestBurger-%s" % location]
+      "BestPizza-%s" % location,
+      "BestRamen-%s" % location,
+      "BestPub-%s" % location,
+      "BestBurger-%s" % location
+  ]
+
+
 """Returns a list of restaurants near gps_coordinate."""
 
 
 def GetGPSCoordinateOf(name: str) -> Tuple[float, float]:
   pass
+
+
 """Returns a GPS coordinate for a city."""
 
 
 def GetWeatherDescriptionAt(gps_coordinate: Tuple[float, float]) -> str:
   pass
+
+
 """Returns a summary describing the most recent weather conditions at a GPS coordinate."""
+
 
 def AccessWikipediaArticle(article_name: str) -> str:
   return "Wikipedia information about %s" % article_name
+
+
 """Returns information about article_name's Wikipedia article."""
 
 # Under the hood this is running an LLM to answer question given article
 
+
 def AnswerQuestionFromArticle(article: str, question: str) -> Any:
   return 100
+
+
 """Answers a question using information from a Wikipedia article."""
