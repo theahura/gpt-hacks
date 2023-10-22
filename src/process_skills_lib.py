db1332c57de29a96a14c8a5f23ee7d58dd7abd2d
Andrew Liu <andrew.liu6d@gmail.com>
2023-10-22 19:33:21 -0400

    skill lib append

--- src/process_skills_lib.py
+++ src/process_skills_lib.py
@@ -15,12 +15,13 @@ def ProcessSkillFile(file_py: str) -> List[Tuple]:
   for fn in fn_code[1:]:
     fn_name = fn.split("(")[0]
     fn_args = "(" + fn.split("(")[1].split("\n")[0][:-1]
-    descriptor = fn.split("""\"\"\"""")[1].strip()
+    descriptor = fn.split("""\"\"\"""")[-2].strip()
 
     found_skills.append((fn_name, fn_args, descriptor))
   return found_skills
 
 def WriteContextForSkillFile(skills:List[Tuple]) -> str:
+  # Oudated and defer to SkillsEmbedding later
   context = """You have the following Python library, their pytype function signature, and description:"""
   context += "\n```"
   for skill in skills:
@@ -31,24 +32,23 @@ def WriteContextForSkillFile(skills:List[Tuple]) -> str:
   return context
 
 class SkillsEmbedding:
-  def __init__(self, base_skill_file, learned_skill_files=None):
+  def __init__(self, base_skill_file, learned_skill_file):
+    self.base_skill_file = base_skill_file
+    self.learned_skill_file = learned_skill_file
+
     self.chroma_client = chromadb.Client()
     self.collection = self.chroma_client.create_collection(name="skill_library")
-    
+
     self.base_skills = ProcessSkillFile(base_skill_file)
-
-    if learned_skill_files:
-      self.learned_skills = ProcessSkillFile(learned_skill_files)
-      # Embed only learned skills
-      self.add_skills_file(learned_skill_files)
-    else:
-      self.learned_skills = []
-
+    self.learned_skills = ProcessSkillFile(learned_skill_file)
+    self.add_skills_file(self.learned_skill_file)
     self.skills = self.base_skills + self.learned_skills
 
   def add_skills_file(self, skill_file):
     # fn_name, fn_args, descriptor
     skills = ProcessSkillFile(skill_file)
+    if len(skills) == 0:
+      return
     skill_file_descriptor = ["%s %s" % (s[2], s[0]) for s in skills]
     skill_id = [s[0] for s in skills]
     self.collection.add(documents=skill_file_descriptor, ids=skill_id)
@@ -59,12 +59,15 @@ class SkillsEmbedding:
     res = self.collection.query(query_texts=skill_description, n_results=n_results)
     return res['ids']
 
-  # def write_skill_to_file(self, skill_file, skill_tuple):
-  #   # skill_tuple is of the form [skill_name, skill_description, code_block]
-  #   code_block = skill_tuple[2]
-  #   code_block.split("->")[1].split(":")[1]
-  #   with open(skill_file, 'a') as f:
-  #     f.write()
+  def append_learned_skill_to_file(self, skill_tuple):
+    # skill_tuple is of the form [skill_name, skill_description, code_block]
+    code_block = skill_tuple[2]
+    with open(self.learned_skill_file, 'a') as f:
+      f.write(code_block)
+      f.write("\n\"\"\"%s\"\"\"\n" % skill_tuple[1])
+    # Very lazy as I don't want to reparse this single instance
+    self.learned_skills = ProcessSkillFile(self.learned_skill_file)
+    self.add_skills_file(self.learned_skill_file)
 
   def construct_skill_context(self, learned_ids):
     context = ""
@@ -84,7 +87,10 @@ class SkillsEmbedding:
     context += "\n```"
     return context
 
-s = SkillsEmbedding(base_skill_file="src/base_skills.py")
-# s.add_skills_file()
-s.search("Need Information about Restaurants", 2)
-print(s.construct_skill_context([]))
+# s = SkillsEmbedding(base_skill_file="src/base_skills.py", learned_skill_file="src/learned_skills.py")
+# # s.add_skills_file()
+# s.search("Need Information about Restaurants", 2)
+
+# s.append_learned_skill_to_file(("test_fn_write", "This is a test description",
+#   """def test_fn_write(x: str)->str:
+#   return \"d\""""))
